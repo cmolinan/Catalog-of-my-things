78373522219595733924cecf56e75a5014285802
angeluray <aeucampos@gmail.com>
2022-11-14 18:41:13 -0400

    Set up item class with methods and properties

---
+++ item.rb
@@ -0,0 +1,35 @@
+class Item
+  attr_accessor :genre, :author, :source, :label, :publish_date, :archived
+
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
+
+  def move_to_archive
+    @archived = can_be_archived? == true
+  end
+
+  private
+
+  def can_be_archived?
+    Time.new.year - publish_date > 10
+  end
+end
