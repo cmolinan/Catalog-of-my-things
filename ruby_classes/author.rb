c8a7858778949cccea0159cb8aa29fb50c483a56
angeluray <aeucampos@gmail.com>
2022-11-15 13:09:35 -0400

    Fix linters

--- ruby_classes/author.rb
+++ ruby_classes/author.rb
@@ -1,14 +1,16 @@
 class Author
-    attr_accessor :id, :first_name, :last_name, :items
-    def initialize(first_name, last_name)
-        id = Random.rand(1..1000)
-        @first_name = first_name
-        @last_name = last_name
-        @items = []
-    end
+  attr_accessor :first_name, :last_name, :items
+  attr_reader :id
 
-    def add_item(item)
-        @items << item
-        item.add_author(self)
-    end
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_author(self)
+  end
 end
--- ruby_classes/game.rb
+++ ruby_classes/game.rb
@@ -1,16 +1,17 @@
 require_relative './item'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
-    def initialize(multiplayer, last_played_at, *args)
-        super(*args)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  attr_accessor :multiplayer, :last_played_at
 
-    private
+  def initialize(multiplayer, last_played_at, *args)
+    super(*args)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    def can_be_archived?
-        super && Time.new.year - last_played_at > 2
-    end
+  private
+
+  def can_be_archived?
+    super && Time.new.year - last_played_at > 2
+  end
 end
