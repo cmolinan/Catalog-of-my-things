26d1d3197a6416bb71f123a8d2983545adfb5c4c
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-15 10:38:53 +0300

    Fix linter/rubocop errors

--- ruby_classes/music_genre.rb
+++ ruby_classes/music_genre.rb
@@ -1,14 +1,14 @@
 class Genre
-    attr_accessor :archived, :items, :name
+  attr_accessor :archived, :items, :name
 
-    def initialize(name)
-        @id = Random.rand(1..1000)
-        @name = name
-        @items = []
-      end
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
 
-      def add_item(item)
-        @items.push(item)
-        item.add_genre(self)
-      end
+  def add_item(item)
+    @items.push(item)
+    item.add_genre(self)
+  end
 end
