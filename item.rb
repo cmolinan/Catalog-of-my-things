dfc9090f79e996ca0bc483978863db00009b2c1b
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-15 09:32:45 +0300

    Update constructors

--- item.rb
+++ item.rb
@@ -1,7 +1,7 @@
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(publish_date)
+  def initialize(id:,publish_date: archived: false)
     @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = false
