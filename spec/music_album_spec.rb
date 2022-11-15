c237a8b4efdc28e69ad0cb41a0ad5db6766cea61
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-15 22:37:43 +0300

    Add extra test example based on review

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -15,3 +15,35 @@ describe MusicAlbum do
     end
   end
 end
+
+describe MusicAlbum do
+  context 'When MusicAlbum is on_spotity to true' do
+    music_album_three = MusicAlbum.new('name', Time.new(11 / 14 / 2022), true)
+    it 'Should return an instance of the Class musicAlbum' do
+      expect(music_album_three).to be_an_instance_of MusicAlbum
+    end
+  end
+
+  context 'When MusicAlbum is on_spotity to false' do
+    music_album_four = MusicAlbum.new('name', Time.new(11 / 14 / 2022), false)
+    it 'Should return an instance of the Class musicAlbum' do
+      expect(music_album_four).to be_an_instance_of MusicAlbum
+    end
+  end
+end
+
+describe MusicAlbum do
+  context 'When MusicAlbum is on_spotity to true' do
+    music_album_five = MusicAlbum.new('name', Time.new(11 / 14 / 2022), true)
+    it 'Should return an instance of the Class musicAlbum' do
+      expect(music_album_five).to be_an_instance_of MusicAlbum
+    end
+  end
+
+  context 'When MusicAlbum is on_spotity to false' do
+    music_album_six = MusicAlbum.new('name', Time.new(11 / 14 / 2022), false)
+    it 'Should return an instance of the Class musicAlbum' do
+      expect(music_album_six).to be_an_instance_of MusicAlbum
+    end
+  end
+end
