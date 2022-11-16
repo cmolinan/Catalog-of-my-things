6ff7f5fbe083a1e4d10cb5cb9c23ce209ff43174
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 10:07:58 +0300

    Implement IO to save and preserve data in json

--- modules/music_album_module.rb
+++ modules/music_album_module.rb
@@ -15,6 +15,10 @@ module MusicAlbumDataController
   end
 
   def save_albums
-    
+    data = []
+    @music_albums.each do |album|
+      data.push({ id: album.id, name: album.name, publish_date: album.publish_date, spotify: album.on_spotify })
+    end
+    open('../json_files/album.json', 'w') { |f| f << JSON.generate(data) }
   end
 end
