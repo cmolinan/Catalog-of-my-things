80c1bb1320e4129d418016de2d928ba1a468c5d5
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 11:26:08 +0300

    Add a music album

--- app.rb
+++ app.rb
@@ -49,7 +49,21 @@ class App
 
   def add_book; end
 
-  def add_music_album; end
+  def add_music_album
+    print 'Please, type the album name: '
+    name = gets.chomp
+
+    print 'Date of publish [Enter date in format (yyyy-mm-dd)]: '
+    publish_date = get_date_from_user(gets.chomp)
+    return unless publish_date
+
+    print 'Has present in spotify? [Y/N]: '
+    on_spotify = gets.chomp.downcase == 'y' || false
+
+    @music_albums << MusicAlbum.new(name, publish_date, on_spotify)
+    puts 'Album created successfully'
+  
+  end
 
   def add_game; end
 end
