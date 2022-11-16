aa8e41db56de8b4ce5449a8de639847b1811417e
angeluray <aeucampos@gmail.com>
2022-11-16 19:36:50 -0400

    Fix linters

--- app.rb
+++ app.rb
@@ -64,7 +64,7 @@ class App
   def list_all_authors
     puts "LIST OF AUTHORS:\n\n"
     @authors.each_with_index do |author, index|
-        puts "[#{index}] #{author.first_name} #{author.last_name}"
+      puts "[#{index}] #{author.first_name} #{author.last_name}"
     end
   end
 
