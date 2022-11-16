a956cb4e54d0df0facfbf39dadd1c3ffb5693bd2
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 13:15:31 +0300

    Update item.rb to fix functional issues

--- ruby_classes/item.rb
+++ ruby_classes/item.rb
@@ -1,9 +1,9 @@
 class Item
-  attr_accessor :genre, :author, :source, :label, :publish_date, :archived
-  attr_reader :id
+  attr_reader :genre, :author, :label
+  attr_accessor :publish_date
 
-  def initialize(publish_date, archived: false)
-    @id = Random.rand(1..1000)
+  def initialize(id:, publish_date:, archived: false)
+    @id = id || Random.rand(1..1000)
     @publish_date = publish_date
     @archived = archived
   end
