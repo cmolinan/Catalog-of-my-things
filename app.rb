03634669a6dd3354ae68daa93b6011dbeb0a5714
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 11:21:30 +0300

    Switch handle_action option  to enhance

--- app.rb
+++ app.rb
@@ -17,11 +17,9 @@ class App
   end
 
   def handle_action(option)
-    action_input = { 1 => list_all_books, 2 => list_all_music_album, 3 => list_all_games, 4 => list_all_genres,
-                     5 => list_all_labels, 6 => list_all_authors, 7 => add_book, 8 => add_music_album, 9 => add_game }
     case option
-    when 1..9
-      action_input[option]
+    when 1..10
+      handle_valid_actions(option)
     else
       puts 'Please put a number between 1 and 10'
     end
