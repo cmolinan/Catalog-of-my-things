0cd225b5b96551e6a87467e270ccce0086249ad3
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 10:08:33 +0300

    Fix linter/rubocop errors

--- modules/music_album_module.rb
+++ modules/music_album_module.rb
@@ -9,7 +9,7 @@ module MusicAlbumDataController
         MusicAlbum.new(ID: album['id'], Name: album['name'], Publish_date: album[publish_date],
                        Spotify: album['on_spotify'])
       end
-     else
+    else
       []
     end
   end
