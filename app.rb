3d0dfdff821677903d316f439e36bf122a648b75
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 11:23:07 +0300

    Implement list options for music Album

--- app.rb
+++ app.rb
@@ -27,7 +27,12 @@ class App
 
   def list_all_books; end
 
-  def list_all_music_album; end
+  def list_all_music_album
+    puts 'Music Albums'
+    @music_albums.each do |music_album|
+      puts "Name: #{music_album.name}, Publish Date: #{music_album.publish_date}, On Spotify: #{music_album.on_spotify}"
+    end
+   end
 
   def list_all_games; end
 
