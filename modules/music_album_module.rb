eb47048019dce5d36705b7a5e96f81e60611d6f1
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 10:15:48 +0300

    Update file location and modify some functions

--- modules/music_album_module.rb
+++ modules/music_album_module.rb
@@ -3,11 +3,9 @@ require 'json'
 
 module MusicAlbumDataController
   def load_albums
-    if File.exist?('album.json')
-      JSON.parse(File.read(album.json)).map do |album|
-        publish_date = Times.new.year
-        MusicAlbum.new(ID: album['id'], Name: album['name'], Publish_date: album[publish_date],
-                       Spotify: album['on_spotify'])
+    if File.exist?('./json_files/album.json') && File.read('./json_files/album.json') != ''
+      JSON.parse(File.read('./json_files/album.json')).map do |album|
+        MusicAlbum.new(album['name'], album['publish_date'], album['spotify'])
       end
     else
       []
@@ -17,8 +15,8 @@ module MusicAlbumDataController
   def save_albums
     data = []
     @music_albums.each do |album|
-      data.push({ id: album.id, name: album.name, publish_date: album.publish_date, spotify: album.on_spotify })
+      data.push({ name: album.name, publish_date: album.publish_date, spotify: album.on_spotify })
     end
-    open('../json_files/album.json', 'w') { |f| f << JSON.generate(data) }
+    open('./json_files/album.json', 'w') { |f| f << JSON.generate(data) }
   end
 end
