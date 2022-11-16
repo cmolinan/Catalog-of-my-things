5be83e08a3de003120892af9d8f5ed0a1cdc2dc8
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-16 10:04:10 +0300

    Fix linter errors

--- modules/music_album_module.rb
+++ modules/music_album_module.rb
@@ -2,15 +2,15 @@ require_relative './ruby_classes/music_album'
 require 'json'
 
 module MusicAlbumDataController
-    def load_albums
-        if File.exist?('album.json')
-            JSON.parse(File.read(album.json)).map do |album|
-              publish_date = Times.new.year
-              MusicAlbum.new(ID: album['id'], Name: album['name'], Publish_date: album[publish_date],
-                             Spotify: album['on_spotify'])
-            end
-          else
-            []
-          end
+  def load_albums
+    if File.exist?('album.json')
+      JSON.parse(File.read(album.json)).map do |album|
+        publish_date = Times.new.year
+        MusicAlbum.new(ID: album['id'], Name: album['name'], Publish_date: album[publish_date],
+                       Spotify: album['on_spotify'])
+      end
+    else
+      []
     end
+  end
 end
