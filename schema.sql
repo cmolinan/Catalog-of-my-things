9ea98fa7a452bcd63972ea2fe80142b14afa306e
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-17 17:19:44 +0300

    Create table music_albums

--- schema.sql
+++ schema.sql
@@ -43,3 +43,9 @@ CREATE TABLE game (
   CONSTRAINT fk_authors FOREIGN KEY(id) REFERENCES authors(id)
 );
 
+CREATE TABLE music_albums (
+    id  INT,
+    name VARCHAR(100),
+    on_spotify BOOLEAN,
+    FOREIGN KEY(id) REFERENCES item(id)
+);
